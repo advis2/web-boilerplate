b69e17d849681bf596a416f763e1875a70f4a053
advis <wadvisw@gmail.com>
2026-02-22 17:34:14 +0900

    fix: vercel 포기

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+
+  // GitHub Pages에 배포 시 basePath 설정
+  basePath: process.env.GITHUB_ACTIONS ? '/web-boilerplate' : '',  // `<repo-name>`을 실제 GitHub 레포지토리 이름으로 교체
+  assetPrefix: process.env.GITHUB_ACTIONS ? '/web-boilerplate' : '', // GitHub Pages에서 자산의 경로 설정
+  distDir: 'out',  // Next.js에서 정적 파일을 `out` 디렉토리로 내보내기 위해 설정  
 };
 
 const plugins = [
